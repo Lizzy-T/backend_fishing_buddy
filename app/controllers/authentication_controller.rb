93232d26fd5725072c603add3d8f6ce470d0f17c
github username <github email address>
2019-10-25 13:10:31 -0600

    change authentication controller login JWT to JsonWebToken

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,7 +2,7 @@ class AuthenticationController < ApplicationController
     def login
         @user = User.find_by(username: params[:username])
         if (@user && @user.authenticate(params[:password]))
-            @token = JWT.encode({
+            @token = JsonWebToken.encode({
                 user_id: @user.id,
                 exp: 24.hours.from_now.to_i
             }, Rails.application.secrets.secret_key_base)
